041fadc3758f6bfd61c2d9aa0cd9858bb84ad518
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2025-06-06 16:40:55 +0530

    Remove unused navbar component and clean bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,4 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-=======
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { AppRoutingModule } from './app/app-routing.module';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideServiceWorker } from '@angular/service-worker';
->>>>>>> Stashed changes
-import { environment } from './environments/environment';
-
-if (environment.production) {enableProdMode();}
-import 'zone.js'; //Added for lazy module error in firefox,safari in server.
-import 'hammerjs';
-import { registerLicense } from '@syncfusion/ej2-base';
-registerLicense('ORg4AjUWIQA/Gnt2U1hhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Ud0xiWX9ddH1VQGJd');
-platformBrowserDynamic().bootstrapModule(AppModule)
-  // .then(() => {
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.register('ngsw-worker.js');
-  // }
-  // }
-  .catch(err => console.error(err));
-=======
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(AppRoutingModule.routes),
-    provideAnimations(),
-    CustomPreloadingStrategy,
-    provideHttpClient(),
-    DataapiService,
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: environment.production,
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ]
-}).catch(err => console.error(err));
->>>>>>> Stashed changes
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
@@ -49,6 +9,15 @@ import { environment } from './environments/environment';
 import { CustomPreloadingStrategy } from './app/custom-preloading-strategy';
 import { provideHttpClient } from '@angular/common/http';
 import { DataapiService } from './app/dataapi.service';
+import 'zone.js';
+import 'hammerjs';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('ORg4AjUWIQA/Gnt2U1hhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Ud0xiWX9ddH1VQGJd');
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -59,9 +28,8 @@ bootstrapApplication(AppComponent, {
     DataapiService,
     provideServiceWorker('ngsw-worker.js', {
       enabled: environment.production,
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ]
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 }).catch(err => console.error(err));
-  
- 
+
